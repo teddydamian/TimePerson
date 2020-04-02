8fd8048eafba738b56d0cd6daa8be3a6d68224e1
teddydamian <teddy.damian@gmail.com>
2020-04-02 11:43:37 -0700

    added a lot of comments

--- TimePerson/Models/TimePerson.cs
+++ TimePerson/Models/TimePerson.cs
@@ -9,6 +9,9 @@ namespace TimePerson.Models
 {
     public class TimePeople
     {
+        /// <summary>
+        /// Properties for TimePeople
+        /// </summary>
         public int Year { get; set; }
         public string Honor { get; set; }
         public string Name { get; set; }
@@ -19,7 +22,9 @@ namespace TimePerson.Models
         public string Category { get; set; }
         public string Context { get; set; }
 
-
+        /// <summary>
+        /// Instantiation of class TimePeople passing arguments in
+        /// </summary>
         public TimePeople (int year, string honor, string name, string country, int birthyear, int deathyear, string title, string category, string context)
         {
             Year = year;
@@ -34,19 +39,22 @@ namespace TimePerson.Models
 
         }
 
+        /// <summary>
+        /// Get Person method will grab information from the list of people by accessing the CSV file.
+        /// Then grab the data, assign it to Object of TimePeople.
+        /// Then querying the list using startYear and endYear.
+        /// </summary>
+        /// <param name="startYear"></param>
+        /// <param name="endYear"></param>
+        /// <returns></returns>
         public static List<TimePeople> GetPersons(int startYear, int endYear)
         {
-
-            //string filepath = @"../TimePerson/wwwroot/personOfTheYear.csv";
 
             string path = Environment.CurrentDirectory;
             string newPath = Path.GetFullPath(Path.Combine(path, @"wwwroot/personOfTheYear.csv"));
             string[] allList = System.IO.File.ReadAllLines(newPath);
 
-
             List<TimePeople> listOfTimePerson = new List<TimePeople>();
-
-            
 
             // Table Data
             for (int i = 1; i < allList.Length; i++)
@@ -55,22 +63,20 @@ namespace TimePerson.Models
 
                 TimePeople people = new TimePeople
                     (
-                    peopleStats[0] == "" ? 0 : Convert.ToInt32(peopleStats[0]), //year
-                    peopleStats[1], //honor
-                    peopleStats[2], //name
-                    peopleStats[3], //country
-                    peopleStats[4] == "" ? 0 : Convert.ToInt32(peopleStats[4]), //birthyear
-                    peopleStats[5] == "" ? 0 : Convert.ToInt32(peopleStats[5]), //deathyear
-                    peopleStats[6], //title
-                    peopleStats[7], //category
-                    peopleStats[8] //context
+                        peopleStats[0] == "" ? 0 : Convert.ToInt32(peopleStats[0]), //year
+                        peopleStats[1], //honor
+                        peopleStats[2], //name
+                        peopleStats[3], //country
+                        peopleStats[4] == "" ? 0 : Convert.ToInt32(peopleStats[4]), //birthyear
+                        peopleStats[5] == "" ? 0 : Convert.ToInt32(peopleStats[5]), //deathyear
+                        peopleStats[6], //title
+                        peopleStats[7], //category
+                        peopleStats[8] //context
                     );
-
-             
-                    listOfTimePerson.Add(people);
-                
+                listOfTimePerson.Add(people);
             }
 
+            //Query to the list
             List<TimePeople> query2 = listOfTimePerson.Where(x => x.Year <= endYear && x.Year >= startYear).ToList();
             return query2;
         }
